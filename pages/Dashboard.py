5bbd0083233eee8a4f3062d747cad1a4583a7066
vishal00v72 <rktjaiswal@gmail.com>
2025-08-17 15:54:42 +0530

    UDashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -6,11 +6,14 @@ st.set_page_config(layout="wide") # Important: Use this on all pages for consist
 st.title("📊 Data Profile Report")
 st.write("Explore the dataset used for model training.")
 
+file_path = r'C:\Users\uniqu\PYkalp\pages\churnA.html'
+
+
 try:
-    with open("churnA.html", "r", encoding="utf-8") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         html_content = f.read()
     
-    st.components.v1.html(html_content, height=800, scrolling=True)
+    components.v1.html(html_content, height=800, scrolling=True)
 
 except FileNotFoundError:
     st.error("Error: 'churnA.html' not found.")
