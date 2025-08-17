c2a435dfe877bfa5eb0f3332d3df8e28a5408adf
vishal00v72 <rktjaiswal@gmail.com>
2025-08-18 00:59:28 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="Churn Predictin ML Web app", page_icon="🎰")
 st.title("Welcome to the Churn Prediction App!")
 st.write("This app allows you to predict customer churn and view the data profile.")
 st.write("👈 Navigate to the pages on the left to get started.")
